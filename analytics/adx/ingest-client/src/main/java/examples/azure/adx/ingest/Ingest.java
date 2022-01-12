30f5f09f5185fb2144411f161bfa537007afae2a
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-01-12 12:10:36 +0100

    Check that the environment variables are set

--- analytics/adx/ingest-client/src/main/java/examples/azure/adx/ingest/Ingest.java
+++ analytics/adx/ingest-client/src/main/java/examples/azure/adx/ingest/Ingest.java
@@ -13,15 +13,44 @@ import java.io.ByteArrayInputStream;
 public class Ingest {
 
     public static void main(String[] arguments) throws Exception {
+        // Checks the environment variables
         String endpoint = System.getenv("ADX_ENDPOINT");
+        if (endpoint == null || endpoint.isEmpty()) {
+            System.out.println("You need to set the variable ADX_ENDPOINT");
+            return;
+        }
         String clientId = System.getenv("ADX_CLIENT_ID");
+        if (clientId == null || clientId.isEmpty()) {
+            System.out.println("You need to set the variable ADX_CLIENT_ID");
+            return;
+        }
         String clientSecret = System.getenv("ADX_CLIENT_SECRET");
+        if (clientSecret == null || clientSecret.isEmpty()) {
+            System.out.println("You need to set the variable ADX_CLIENT_SECRET");
+            return;
+        }
         String tenantId = System.getenv("ADX_TENANT_ID");
+        if (tenantId == null || tenantId.isEmpty()) {
+            System.out.println("You need to set the variable ADX_TENANT_ID");
+            return;
+        }
+        String databaseName = System.getenv("ADX_DATABASE_NAME");
+        if (databaseName == null || databaseName.isEmpty()) {
+            System.out.println("You need to set the variable ADX_DATABASE_NAME");
+            return;
+        }
+        String table = System.getenv("ADX_TABLE");
+        if (table == null || table.isEmpty()) {
+            System.out.println("You need to set the variable ADX_TABLE");
+            return;
+        }
+
+        // Ingests from stream
         ConnectionStringBuilder connectionStringBuilder = ConnectionStringBuilder.createWithAadApplicationCredentials(endpoint, clientId, clientSecret, tenantId);
         IngestClient client = IngestClientFactory.createStreamingIngestClient(connectionStringBuilder);
         String content = System.currentTimeMillis() + ",'1234567890'";
         StreamSourceInfo streamSourceInfo = new StreamSourceInfo(new ByteArrayInputStream(content.getBytes()));
-        IngestionProperties ingestionProperties = new IngestionProperties(System.getenv("ADX_DATABASE_NAME"), System.getenv("ADX_TABLE"));
+        IngestionProperties ingestionProperties = new IngestionProperties(databaseName, table);
         IngestionResult result = client.ingestFromStream(streamSourceInfo, ingestionProperties);
         IngestionStatus status = result.getIngestionStatusCollection().get(0);
         while (status.status == OperationStatus.Pending) {
