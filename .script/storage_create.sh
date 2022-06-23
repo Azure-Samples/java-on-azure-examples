72401e8a7805b71a5c0c2b6fa456cc50d880dee6
Automated workflow <noreply@microsoft.com>
2022-06-23 19:00:37 +0000

    Committing workflows

---
+++ .script/storage_create.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd ..
+
+
+if [[ -z $RESOURCE_GROUP ]]; then
+export RESOURCE_GROUP=java-on-azure-$RANDOM
+export REGION=southcentralus
+fi
+
+az group create --name $RESOURCE_GROUP --location $REGION
+export STORAGE_ACCOUNT_NAME=storage$RANDOM
+
+az storage account create \
+--name $STORAGE_ACCOUNT_NAME \
+--resource-group $RESOURCE_GROUP \
+--sku Standard_LRS \
+--kind StorageV2
+
+export STORAGE_ACCOUNT_CONNECTION_STRING=$(az storage account show-connection-string --resource-group $RESOURCE_GROUP --name $STORAGE_ACCOUNT_NAME --output tsv)
+export RESULT=$(az storage account show --resource-group $RESOURCE_GROUP --name $STORAGE_ACCOUNT_NAME --query provisioningState --output tsv)
+az group delete --name $RESOURCE_GROUP --yes || true
+if [[ "$RESULT" != Succeeded ]]; then
+exit 1
+fi
