b09990c454bda8096f34036122081bad2e02e876
Manfred Riem <manfred.riem@microsoft.com>
2023-12-12 11:22:59 -0600

    Fixes #389 - Fix 'Deploy a Hello World Spring Boot application' example (#390)

--- spring/helloworld/src/main/java/helloworld/HelloWorldApplication.java
+++ spring/helloworld/src/main/java/helloworld/HelloWorldApplication.java
@@ -2,10 +2,8 @@ package helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class HelloWorldApplication {
 
     public static void main(String[] arguments) {
