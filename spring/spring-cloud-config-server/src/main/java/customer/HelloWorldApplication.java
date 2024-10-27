be15e85022282b30dd54b515dfd9c096b15b0282
karthirm <karthirm90@gmail.com>
2024-10-27 21:35:53 +0400

    Configure centralized Azure Spring Cloud Config Server

---
+++ spring/spring-cloud-config-server/src/main/java/customer/HelloWorldApplication.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+    public static void main(String[] arguments) {
+        SpringApplication.run(HelloWorldApplication.class, arguments);
+    }
+}
