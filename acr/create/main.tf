423e4c9fdb977d64042de4811fd3e64431c57ba5
Manfred Riem <manfred.riem@microsoft.com>
2025-01-07 09:15:25 -0600

    Updated 'Create an Azure Container Registry' example

--- acr/create/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_container_registry" "joazcr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.joazrg.name
-  location            = azurerm_resource_group.joazrg.location
-  sku                 = var.acr_sku
-  admin_enabled       = true
-}
--- acr/create/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "acr_name" {
-  description = "The name of the Azure Container Registry"
-  value       = azurerm_container_registry.joazcr.name
-}
-
-output "acr_sku" {
-  description = "The SKU of the Azure Container Registry"
-  value       = azurerm_container_registry.joazcr.sku
-}
