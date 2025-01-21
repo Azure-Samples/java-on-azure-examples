8fc8a6c96ac3af5537a982f43175c0b7622e5fe9
Manfred Riem <manfred.riem@microsoft.com>
2025-01-21 12:18:51 -0600

    Fixes #532 - Fix 'Deploy a Spring Boot application' example (#533)

--- acr/springboot/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "acr_springboot_image_name" {
-  description = "The name of the Docker image"
-  value       = var.acr_springboot_image_name
-}
-
-output "acr_springboot_build_output" {
-  description = "The output of the ACR build command"
-  value       = local_file.acr_springboot_build_output.content
-}
--- acr/springboot/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "acr_springboot_image_name" {
-  description = "The name of the Docker image to be built and pushed"
-  type        = string
-  default     = "springboot:latest"
-}
