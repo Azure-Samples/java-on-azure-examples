b76f52454a4d0f35e54238c2ad3aecd78300ce02
Manfred Riem <manfred.riem@microsoft.com>
2025-01-24 08:28:26 -0600

    Updated appservice/create-plan/README.md workflow

--- appservice/create-plan/README.md
+++ appservice/create-plan/README.md
@@ -12,7 +12,7 @@
   fi
 
   -->
-<!-- workflow.cron(0 0 * * 3) -->
+<!-- workflow.cron(0 0 * * 4) -->
 <!-- workflow.include(../../group/create/README.md) -->
 
 This example assumes you have previously completed the following example:
--- appservice/create-plan/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "app_service_plan_tier" {
-  description = "The pricing tier for the App Service Plan"
-  default     = "PremiumV3"
-}
-
-variable "app_service_plan_size" {
-  description = "The instance size for the App Service Plan"
-  default     = "P1v3"
-}
